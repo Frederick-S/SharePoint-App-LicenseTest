1d40969f140c3b55aa58fedc309f5af7c66acfe9
Frederick-S <mao_xiaodan@hotmail.com>
2015-03-20 15:50:45 +0800

    Import test license to sharepoint online

---
+++ Features/Feature/Feature.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="0f79ba71-e9d1-4710-b087-cd53086980af" featureId="0f79ba71-e9d1-4710-b087-cd53086980af" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SharePoint.App.LicenseTest Feature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="55c923c8-3801-44b6-bb8a-2506dd320a1c" />
+    <projectItemReference itemId="e18c57ac-353d-440a-9dec-d4dd67f9aab2" />
+    <projectItemReference itemId="35d9ce72-f56b-43de-ae8e-c34f4c885667" />
+    <projectItemReference itemId="10d94731-f491-4a22-86fd-5c4235ac75c2" />
+  </projectItems>
+</feature>
---
+++ Scripts/App.js
@@ -0,0 +1,33 @@
+﻿(function ($) {
+    function importTestLicense() {
+        // Use the product id defined in AppManifest.xml
+        var productId = '{55c923c8-3801-44b6-bb8a-2506dd320a1c}';
+        var rawXMLLicenseToken =
+            '<r>' +
+                '<t ' +
+                'aid="WA900006056" ' +
+                'pid="' + productId + '" ' +
+                'cid="32F3E7FC559F4F49" ' +
+                'ts="30" ' +
+                'et="Trial" ' +
+                'ad="2012-10-30T16:49:34Z" ' +
+                'ed="2012-10-30T16:49:34Z" ' +
+                'sd="2012-10-30T16:49:34Z" ' +
+                'te="2012-10-30T16:49:34Z" ' +
+                'test="true"' +
+                '/>' +
+                '<d>VNNAnf36IrkyUVZlihQJNdUUZl/YFEfJOeldWBtd3IM=</d>' +
+            '</r>';
+
+        var context = SP.ClientContext.get_current();
+        SP.Utilities.Utility.importAppLicense(context, rawXMLLicenseToken, 'en-US', 'US', 'App Name', 'http://www.office.com', 'Provider Name', 5);
+
+        context.executeQueryAsync(function (sender, args) {
+            alert('License is imported successfully.');
+        }, function (sender, args) {
+            alert(args.get_message());
+        });
+    }
+
+    importTestLicense();
+})(jQuery);
