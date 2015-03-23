75b3b7fe01235c205b14a13015a568171e39f9f5
Frederick-S <mao_xiaodan@hotmail.com>
2015-03-23 10:09:02 +0800

    Validate license token with SharePoint Store verification web service

--- Scripts/App.js
+++ Scripts/App.js
@@ -10,10 +10,10 @@
                 'cid="32F3E7FC559F4F49" ' +
                 'ts="30" ' +
                 'et="Trial" ' +
-                'ad="2012-10-30T16:49:34Z" ' +
-                'ed="2012-10-30T16:49:34Z" ' +
-                'sd="2012-10-30T16:49:34Z" ' +
-                'te="2012-10-30T16:49:34Z" ' +
+                'ad="2015-03-01T16:49:34Z" ' +
+                'ed="2015-03-30T16:49:34Z" ' +
+                'sd="2015-03-01T16:49:34Z" ' +
+                'te="2015-03-30T16:49:34Z" ' +
                 'test="true"' +
                 '/>' +
                 '<d>VNNAnf36IrkyUVZlihQJNdUUZl/YFEfJOeldWBtd3IM=</d>' +
@@ -23,7 +23,33 @@
         SP.Utilities.Utility.importAppLicense(context, rawXMLLicenseToken, 'en-US', 'US', 'App Name', 'http://www.office.com', 'Provider Name', 5);
 
         context.executeQueryAsync(function (sender, args) {
-            alert('License is imported successfully.');
+            console.log('License is imported successfully.');
+
+            var appLicenseCollection = SP.Utilities.Utility.getAppLicenseInformation(context, productId);
+
+            context.executeQueryAsync(function (sender, args) {
+                if (appLicenseCollection.get_count() > 0) {
+                    var rawXMLLicenseToken = appLicenseCollection.get_item(0).get_rawXMLLicenseToken();
+                    var rawXMLLicenseTokenEncoded = encodeURIComponent(rawXMLLicenseToken);
+
+                    var verificationServiceUrl = "https://verificationservice.officeapps.live.com/ova/verificationagent.svc/rest/verify?token=";
+                    var request = new SP.WebRequestInfo();
+                    request.set_url(verificationServiceUrl + rawXMLLicenseTokenEncoded);
+                    request.set_method("GET");
+                    var response = SP.WebProxy.invoke(context, request);
+                    context.executeQueryAsync(function () {
+                        if (response.get_statusCode() == 200) {
+                            alert(response.get_body());
+                        } else {
+                            alert(response.get_body());
+                        }
+                    }, function () {
+                        alert(response.get_body());
+                    });
+                }
+            }, function (sender, args) {
+                alert(args.get_message());
+            });
         }, function (sender, args) {
             alert(args.get_message());
         });
